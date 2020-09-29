de2bc06be3cd2f7f954b2ea4e25caa524a11e368
Sahil Srivastava <71905741+previouslysahil@users.noreply.github.com>
2020-09-28 20:26:26 -0500

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,18 @@
+// --== CS400 File Header Information ==--
+// Name: Sahil Srivastava
+// Email: srivastava34@wisc.edu
+// Team: KF
+// TA: Sid
+// Lecturer: Gary Dahl
+// Notes to Grader: <optional extra notes>
+
+public class Node<K, V> {
+
+    public K key;
+    public V value;
+
+    Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
